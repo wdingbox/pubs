5e284054c49f37133935553d169b25e0a8f3407a
wdingsoft <wdingbox@gmail.com>
2022-10-24 16:28:48 -0400

    journey_data.js

--- biblestudy/perimeterChurch/group/journey/2022/MySpiritualJourneySoFar/journey_data.js
+++ biblestudy/perimeterChurch/group/journey/2022/MySpiritualJourneySoFar/journey_data.js
@@ -1,15 +1,14 @@
 
 
 var journey_data =
-
 {
     "1964": [
-        "0",
-        "According to my mother's account, I was born in Beijing Xuanwu Hospital in 1964. The hospital is close to The Xuanwu Gate Catholic Church that was built by Matteo Ricci in 1605. (http://oa.upm.es/33835/1/SHAN_HUANG.pdf).  I visited both sites in Dec 2019."
+        "0.0",
+        "Born in Beijing Xuanwu Hospital in 1964, which is close to The Xuanwu Gate Catholic Church that was built by Matteo Ricci in 1605. (http://oa.upm.es/33835/1/SHAN_HUANG.pdf).  Visited both sites in Dec 2019."
     ],
     "1965": [
-        "-47",
-        "In Oct at age of one, due to the movement of the cultural revolution, I was sent to my grandmom's family in a small Buddha town at Yiwu of Zhejiang Province."
+        "-47.0",
+        "sent to a small Buddha town at Yiwu of Zhejiang Province to live with my grandma in Oct. "
     ],
     "1966": [
         "",
@@ -36,8 +35,8 @@ var journey_data =
         ""
     ],
     "1972": [
-        "-70.76923076923077",
-        "back to Beijing to live w/ parents"
+        "-70.8",
+        "carried back to Beijing to live w/ parents"
     ],
     "1973": [
         "",
@@ -77,7 +76,7 @@ var journey_data =
     ],
     "1982": [
         "-46.2",
-        "College life. during the period, influenced by Chinese Daoism(Laozi/Zhuangzi).(2) in 1986, I continued to pursue the master's degree major in vibration control."
+        "started college life far away from parents, influenced by Chinese Daoism(Laozi/Zhuangzi).(2) in 1986, continue the master's degree major in vibration control."
     ],
     "1983": [
         "",
@@ -112,12 +111,12 @@ var journey_data =
         ""
     ],
     "1991": [
-        "-40",
-        "(1)Left CISC. (2) UIC trainning in Binghamton NY."
+        "-40.0",
+        "(1)Left CISC. (2) UIC short training in Binghamton NY."
     ],
     "1992": [
-        "-74",
-        "marriage. visited USA,"
+        "-74.0",
+        "marriage. visited USA."
     ],
     "1993": [
         "",
@@ -129,7 +128,7 @@ var journey_data =
     ],
     "1995": [
         "-29.7",
-        "USA dream."
+        "began USA dream."
     ],
     "1996": [
         "",
@@ -141,7 +140,7 @@ var journey_data =
     ],
     "1998": [
         "-25.1",
-        "First job"
+        "First job."
     ],
     "1999": [
         "",
@@ -153,14 +152,14 @@ var journey_data =
     ],
     "2001": [
         "-22.6",
-        "(1) got citizenship. (2)laid off"
+        "(1) naturalized citizen. (2)laid off."
     ],
     "2002": [
-        "-80",
-        "stayed at home"
+        "-80.0",
+        "stayed at home."
     ],
     "2003": [
-        "-100",
+        "-100.0",
         "(1) family conflict. (2) found a job in Ligos Software in Savannah GA."
     ],
     "2004": [
@@ -212,7 +211,7 @@ var journey_data =
         ""
     ],
     "2016": [
-        "100",
+        "100.0",
         "(1) quitted Emerson in Huntsville to go to CIU. (2) visited Beijing in Dec 2019.(3) on the crossroad in the missionary: My vision: 'Leave your country, your people and your father's household and go to the land I will show you.'  (Gen12:1 NIV)"
     ],
     "2017": [
@@ -224,7 +223,7 @@ var journey_data =
         ""
     ],
     "2019": [
-        "71",
+        "71.0",
         "graduated fr CIU. Visit BJ with mission. "
     ],
     "2020": [
@@ -232,16 +231,16 @@ var journey_data =
         ""
     ],
     "2021": [
-        "36",
+        "36.0",
         "pandemic serve in school,"
     ],
     "2022": [
-        "69.74358974358975",
+        "69.7",
         "(1)Journey in Beijing (2)missionary calling."
     ],
     "2023": [
-        "30",
-        "doomsday future"
+        "30.0",
+        "future missionary"
     ],
     "2024": [
         "",
