631e00a8423a32762966da555d03c99708479c60
wdingsoft <wdingbox@gmail.com>
2022-11-01 16:18:15 -0400

    modified:   ccahs/ccahs27.htm modified:   ccahs/js/ccahs27.js modified:   ccahs/js/ccahs_uti.js modified:   chinese_characteristics_arthur_henderson_smith_contents.htm modified:   chinese_characteristics_arthur_henderson_smith_full.htm modified:   index.htm

--- biblestudy/my_papers/dhtm/family_missionary/chinese_charateristics/ccahs/js/ccahs27.js
+++ biblestudy/my_papers/dhtm/family_missionary/chinese_charateristics/ccahs/js/ccahs27.js
@@ -7,10 +7,38 @@
 
 //当中国人开始更多地了解西方文明的时候，他们所了解的只是西方人强迫他们接受的西方文明中最坏的成分
 
+var ccahs27 = 
+{
+    "保全面子 face": "steal in order to save face",
+    "节俭持家 economy": "steal in order to save money",
+    "勤劳刻苦 industry": "willing to make full effort at stall.",
+    "讲究礼貌 pliteness": "steal in order to show pliteness",
+    "漠视时间 the disregard of time": "disregard of time to steal at stall",
+    "漠视精确 the disregard of accuracy": "confuse people in order to steal",
+    "易于误解 the talent for misunderstanding ": "misunderstand in order to get pretext to steal",
+    "拐弯抹角 the talent for indirection": "steal for indirection",
+    "顺而不从 flexible inflexibility": "flexible in steal.&nbsp;",
+    "思绪含混 intellectual turbidity": "make people comfusing in order to steal",
+    "不紧不慢 the absence of nerves": "steal without noticing slowly.",
+    "轻视外族 contempt for foreigners": "steal those who are stupid and careless",
+    "缺乏公心 the absence of public spirit": "",
+    "因循守旧 conservatism": "",
+    "随遇而安 indifference to comfort and convenience": "flexible in steal, if can steal then do it, if not ok.",
+    "顽强生存 physical vitality": "for living",
+    "能忍且韧 patience and perseverance": "never change",
+    "知足常乐 content and cheerfulness": "flexible in stealing, if can steal then do it, if not ok",
+    "孝悌为先 filial piety": "pretend to be good.",
+    "仁爱之心 benevolence ": "",
+    "缺乏同情 the absence of sympathy": "",
+    "社会风波 social typhhoons": "",
+    "诛连守法 mutual responsibility and respect for law": "",
+    "相互猜疑 mutual suspicion": "",
+    "缺乏诚信 the absence of sinerity ": "",
+    "多元信仰 polytheism, pantheism, atheism.": "",
+    "中国的现实与时务 the real condition of China and her present needs": ""
+}
 
-
-
-var ccahs27 = {
+var ccahs27_obj = {
     "保全面子 face": [""],
     "节俭持家 economy": [""],
     "勤劳刻苦 industry": [""],
--- biblestudy/my_papers/dhtm/family_missionary/chinese_charateristics/ccahs/js/ccahs_uti.js
+++ biblestudy/my_papers/dhtm/family_missionary/chinese_charateristics/ccahs/js/ccahs_uti.js
@@ -4,30 +4,48 @@
 
 
 var ccahs_uti = {
-    gen_tab: function (obj) {
+    gen_table: function (obj) {
+        ccahs_uti.m_obj = obj
         ccahs_uti.gen_tab_elm(obj)
         ccahs_uti.gen_tab_evt()
     },
     gen_tab_evt: function () {
-        $("td").on("click",function(){
+        $("td").on("click", function () {
             $(this).toggleClass("hili")
         })
-        
+
+        $("textarea").on("click", function () {
+            ccahs_uti.get_editable_dat_from_table()
+        })
+
     },
     gen_tab_elm: function (obj) {
         var stb = "<table border='1'><thead><tr><th>#</th><th>Item</th><th>Desc</th></tr></thead><tbody>"
         var ar = Object.keys(obj)
         ar.forEach(function (key, i) {
-            stb += `<tr><td>${1+i}</td><td>${key}</td><td contenteditable>${obj[key]}</td></tr>`
+            stb += `<tr><td>${1 + i}</td><td>${key}</td><td contenteditable='true' id='${key}'>${obj[key]}</td></tr>`
 
 
         })
         stb += `</tbody></table>`
         var e = document.createElement("div")
         e.innerHTML = (stb)
+        var ta = document.createElement("textarea")
 
-        document.getElementsByTagName("body")[0].appendChild(e)
+
+        document.getElementsByTagName("body")[0].appendChild(e).appendChild(ta)
     },
+
+    get_editable_dat_from_table: function () {
+        var obj = ccahs_uti.m_obj
+        var ar = Object.keys(obj)
+        ar.forEach(function (key, i) {
+            var txt = document.getElementById(key).innerHTML
+            obj[key] = txt
+        })
+        var str = JSON.stringify(obj, null, 4)
+        $("textarea")[0].value = str
+    }
 
 
 }
