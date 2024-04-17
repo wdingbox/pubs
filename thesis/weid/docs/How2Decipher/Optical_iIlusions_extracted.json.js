858865e1ad801f9d14bc14fb7b6341b259735729
wdingsoft <wdingbox@gmail.com>
2024-04-17 16:01:25 -0400

    update optical illusion

--- thesis/weid/docs/How2Decipher/Optical_iIlusions_extracted.json.js
+++ thesis/weid/docs/How2Decipher/Optical_iIlusions_extracted.json.js
@@ -808,6 +808,21 @@ var imgsrcObj =
         11,
         "Can You Spot the Hidden Snake Among the Pile of Bananas in the Picture?",
         "https://www.jagranjosh.com/general-knowledge/optical-illusion-iq-test-spot-the-hidden-snake-among-bananas-in-the-picture-1700553450-1"
+    ],
+    "image158.png":[
+        5,
+        "Find the Different Bird in 5 Seconds",
+        "https://www.jagranjosh.com/general-knowledge/optical-illusion-eye-test-find-the-different-bird-1700462845-1?utm_source=taboola&utm_medium=referral&utm_campaign=taboola_feed"
+    ],
+    "image159.png":[
+        11,
+        "Can You Spot Which Hamilton is Different in the Picture Within 11 seconds?",
+        "https://www.jagranjosh.com/general-knowledge/brain-teaser-iq-test-spot-which-hamilton-is-different-in-the-picture-1700472063-1?utm_source=taboola&utm_medium=referral&utm_campaign=taboola_feed"
+    ],
+    "image160.png":[
+        11,
+        "Optical Illusion IQ Test: Use Your Razor-Sharp Observation Skills To Spot A Snake In 8 Seconds!?",
+        "https://www.jagranjosh.com/general-knowledge/optical-illusion-iq-test-spot-the-snake-1713336250-1"
     ]
 
 }
